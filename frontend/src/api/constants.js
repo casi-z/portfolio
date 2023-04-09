fad946cb472689894e7c8c65fa43e7ed49158b9e
casi-z <seryogataran@gmail.com>
2023-04-09 21:40:50 +0300

    изменен gitignore

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -6,4 +6,3 @@ export const API_GIT_REPOS_CONTENTS = `/repos/${API_GIT_USER}/`
 
 export const API_GIT_BASE_CD_URL = 'https://raw.githubusercontent.com'
 export const API_GIT_CONTENT_DOWNLOAD = `/${API_GIT_USER}/`
-console.log(token);
