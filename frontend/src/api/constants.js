618ffb7bb526e1957209c7ca42e9bec32ca88d36
casi-z <seryogataran@gmail.com>
2023-04-10 10:53:10 +0300

    изменён токен

--- frontend/src/api/constants.js
+++ frontend/src/api/constants.js
@@ -3,6 +3,5 @@ export const API_GIT_BASE_URL = 'https://api.github.com'
 export const API_GIT_USER = 'casi-z'
 export const API_GIT_REPOS = `/users/${API_GIT_USER}/repos`
 export const API_GIT_REPOS_CONTENTS = `/repos/${API_GIT_USER}/`
-
 export const API_GIT_BASE_CD_URL = 'https://raw.githubusercontent.com'
 export const API_GIT_CONTENT_DOWNLOAD = `/${API_GIT_USER}/`
